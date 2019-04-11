fed8c473e21bd55996f76f84d92e22f41dff5e1b
test28989 <test28989@gmail.com>
2019-04-11 17:20:52 +0700

    Change search function

--- src/main/java/com/product/prj/repository/CategoriesRepository.java
+++ src/main/java/com/product/prj/repository/CategoriesRepository.java
@@ -1,11 +1,12 @@
 package com.product.prj.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.product.prj.entity.Categories;
 
 @Repository
-public interface CategoriesRepository extends CrudRepository<Categories, Long>{
+public interface CategoriesRepository extends CrudRepository<Categories, Long>, JpaSpecificationExecutor<Categories>{
 
 }
