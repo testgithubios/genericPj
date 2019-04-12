3bf786af86240d0eafaba9896c0ff8ad7538e318
test28989 <test28989@gmail.com>
2019-04-12 13:44:27 +0700

    Change Name

--- src/main/java/com/product/prj/repository/SuppliersTranslateRepository.java
+++ src/main/java/com/product/prj/repository/SuppliersTranslateRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import com.product.prj.entity.SuppliersTranslate;
+
 @Repository
-public interface SuppliersTranslateRepository extends JpaRepository<SuppliersTranslateRepository, Long> , JpaSpecificationExecutor<SuppliersTranslateRepository>{
+public interface SuppliersTranslateRepository extends JpaRepository<SuppliersTranslate, Long> , JpaSpecificationExecutor<SuppliersTranslate>{
 
 }
