1f82d17b15b79c96aa03365e59920bd6166ac5ed
APAC\DMP1HC <Duyen.Pham>
2019-04-16 11:02:25 +0700

    Testing git

--- src/main/java/com/product/prj/specification/CategoriesSpecification.java
+++ src/main/java/com/product/prj/specification/CategoriesSpecification.java
@@ -6,11 +6,8 @@ import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import com.product.prj.entity.*;
 import org.springframework.data.jpa.domain.Specification;
-
-import com.product.prj.entity.Categories;
-import com.product.prj.entity.CategoriesTranslate;
-import com.product.prj.entity.CusLanguages;
 
 public class CategoriesSpecification implements Specification<Categories> {
 
@@ -24,24 +21,20 @@ public class CategoriesSpecification implements Specification<Categories> {
 	@Override
 	public Predicate toPredicate(Root<Categories> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
 		
-		Join<Categories,CategoriesTranslate> joinCategoriesTranslate = root.join("categoriesTranslates");
-		Join<CategoriesTranslate, CusLanguages> joinLanguages = joinCategoriesTranslate.join("languages");
+		Join<Categories,CategoriesTranslate> joinCategoriesTranslate = root.join(Categories_.categoriesTranslates);
+		Join<CategoriesTranslate, CusLanguages> joinLanguages = joinCategoriesTranslate.join(CategoriesTranslate_.languages);
 		if(criteria != null) {
 			if (criteria.getOperation().equalsIgnoreCase(">")) {
-				return builder.greaterThanOrEqualTo(joinCategoriesTranslate.get(criteria.getKey()), criteria.getValue().toString());
+				return builder.greaterThanOrEqualTo(joinLanguages.get(criteria.getKey()), criteria.getValue().toString());
 			} else if (criteria.getOperation().equalsIgnoreCase("<")) {
-				return builder.lessThanOrEqualTo(joinCategoriesTranslate.get(criteria.getKey()), criteria.getValue().toString());
+				return builder.lessThanOrEqualTo(joinLanguages.get(criteria.getKey()), criteria.getValue().toString());
 			} else if (criteria.getOperation().equalsIgnoreCase(":")) {
-				if (joinCategoriesTranslate.get(criteria.getKey()).getJavaType() == String.class) {
-					return builder.like(joinCategoriesTranslate.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+				if (joinLanguages.get(criteria.getKey()).getJavaType() == String.class) {
+					return builder.like(joinLanguages.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
 				} else {
-					return builder.equal(joinCategoriesTranslate.get(criteria.getKey()), criteria.getValue());
+					return builder.equal(joinLanguages.get(criteria.getKey()), criteria.getValue());
 				}
 			}
-			
-//			if(criteria.getOperation().equalsIgnoreCase("languagesId")) {
-//				return builder.equal(joinLanguages.get("languages_id"), criteria.getValue());
-//			}
 		}
 		return null;
 	}
